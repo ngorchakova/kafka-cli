ff43f86e190f4e61e56551090b2e91285fc5f972
Natalia Menshikova <natalia.menshikova@ig.com>
2017-01-17 19:46:20 +0100

    add implementation of describe topic

--- src/main/scala/org/kafka/cli/commands/DescribeTopicAction.scala
+++ src/main/scala/org/kafka/cli/commands/DescribeTopicAction.scala
@@ -1,25 +1,73 @@
 package org.kafka.cli.commands
 
-import org.kafka.cli.{CommandLineActionFactory, CommandLineAction}
+import kafka.admin.AdminUtils
+import kafka.server.ConfigType
+import kafka.utils.ZkUtils
+import org.apache.kafka.common.security.JaasUtils
+import org.kafka.cli.{CommandLineAction, CommandLineActionFactory}
 import scopt.OptionParser
+import java.util.Properties
+import scala.collection.JavaConversions._
 
 /**
- * @author Natalia Gorchakova
- * @since  08.01.2017
- */
+  * @author Natalia Gorchakova
+  * @since 08.01.2017
+  */
 class DescribeTopicAction(val config: DescribeTopicActionConfig) extends CommandLineAction {
-  override def perform() = println(s"describe topic ${config.topic} for ${config.bootstrapServer}")
+
+  override def perform(): Unit = {
+    val zkUtils = ZkUtils(config.zookeeper,
+      30000,
+      30000,
+      JaasUtils.isZkSecurityEnabled)
+
+    zkUtils.getPartitionAssignmentForTopics(List(config.topic)).get(config.topic) match {
+      case Some(topicPartitionAssignment) =>
+        val sortedPartitions = topicPartitionAssignment.toList.sortWith((m1, m2) => m1._1 < m2._1)
+        val configs = AdminUtils.fetchEntityConfig(zkUtils, ConfigType.Topic, config.topic)
+        val liveBrokers = zkUtils.getAllBrokersInCluster().map(_.id).toSet
+
+        if (configs.size() != 0) {
+          val numPartitions = topicPartitionAssignment.size
+          val replicationFactor = topicPartitionAssignment.head._2.size
+          val topicConfiguration = configs.map(kv => kv._1 + "=" + kv._2).mkString(",")
+
+          println(s"Topic: ${config.topic} \t" +
+            s"PartitionCount: $numPartitions \t" +
+            s"ReplicationFactor: $replicationFactor \t" +
+            s"Configs:$topicConfiguration")
+        }
+
+        for ((partitionId, assignedReplicas) <- sortedPartitions) {
+          val inSyncReplicas = zkUtils.getInSyncReplicasForPartition(config.topic, partitionId)
+          val leader = zkUtils.getLeaderForPartition(config.topic, partitionId)
+          print("\tTopic: " + config.topic)
+          print("\tPartition: " + partitionId)
+          print("\tLeader: " + (
+            if (leader.isDefined) leader.get + (if (liveBrokers.contains(leader.get)) "" else " (inactive)")
+            else "none"))
+          print("\tReplicas: " + assignedReplicas.mkString(","))
+          println("\tIsr: " + inSyncReplicas.mkString(","))
+
+        }
+
+      case None =>
+        println(s"Topic ${config.topic} doesn't exist!")
+    }
+
+  }
 }
 
-private[commands] case class DescribeTopicActionConfig(bootstrapServer: String = null, topic: String = null)
+private[commands] case class DescribeTopicActionConfig(zookeeper: String = null, topic: String = null)
 
 object DescribeTopicAction extends CommandLineActionFactory {
 
   val Parser: OptionParser[DescribeTopicActionConfig] = new OptionParser[DescribeTopicActionConfig]("describeTopic") {
     opt[String]('t', "topic").required().action((t, c) =>
       c.copy(topic = t)).text("topic name")
-    opt[String]('b', "bootstrap").required().action((s, c) =>
-      c.copy(bootstrapServer = s)).text("bootstrap service list")
+    opt[String]('z', "zookeeper").required().action((s, c) =>
+      c.copy(zookeeper = s)).text("zookeeper connect url")
+
   }
 
   override def createAction(args: Seq[String]): Option[CommandLineAction] = {
